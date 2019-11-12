3959e1217798c66c06c41af6d4e4519a99233c06
kaleempasha493 <kaleempasha493@gmail.com>
2019-11-12 23:58:50 +0530

    update sw 1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
+      body: 'Tommorroe will on instagram',
       icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
@@ -8,14 +8,14 @@ self.addEventListener('push', function(e) {
         primaryKey: '2'
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
+        {action: 'explore', title: 'Remind me later',
           icon: 'images/checkmark.png'},
         {action: 'close', title: 'Close',
           icon: 'images/xmark.png'},
       ]
     };
     e.waitUntil(
-      self.registration.showNotification('Want to redirect google, THEN CLICK !', options)
+      self.registration.showNotification('Today enjoy on whatsup !!', options)
     );
   });
 
@@ -34,10 +34,28 @@ self.addEventListener('push', function(e) {
     if (action === 'close') {
       notification.close();
     } else {
-      clients.openWindow('https://www.google.com');
+      clients.openWindow('https://web.whatsup.com/');
       notification.close();
     }
   });
 
+  self.addEventListener('message', event => {
+    console.log(event);
+    if (event.data === 'skipWaiting') {
+      self.skipWaiting();
+    }
+  });
 
- 
+  self.addEventListener('install', function(event) {
+    // The promise that skipWaiting() returns can be safely ignored.
+    console.log("install");
+    self.skipWaiting();
+
+  });
+
+  self.addEventListener('activate', function(event) {
+    // The promise that skipWaiting() returns can be safely ignored.
+    self.skipWaiting();
+    console.log("activate");
+
+  });
