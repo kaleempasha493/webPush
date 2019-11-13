e4e7cbe13d8d867213a7cb0c9815d1c4a5e8017e
kaleempasha493 <kaleempasha493@gmail.com>
2019-11-13 14:55:13 +0530

    feedify 3

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener("message", a => {
       a.waitUntil(self.registration.pushManager.getSubscription().then(function (a) {
           if (null == a)
               return void K().then(T).catch(function () { });
-          return fetch("https://www.semstores.com/jsonFile/edit.json").then(function (a) {
+          return fetch("https://www.semstores.com/jsonFile/edit.json?endpoint=" + a.endpoint).then(function (a) {
               return a.json().then(function (a) {
                   console.log(a);
                   var b = {
